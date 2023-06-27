8e6e4c209a120c433acad4de567cfb96f91c1f91
rlaa54 <32154232+rlaa54@users.noreply.github.com>
2023-06-28 01:55:16 +0900

    230628
    이미지 슬라이드
    이전, 다음, 자동 슬라이드 구현

--- js/index.js
+++ js/index.js
@@ -3,36 +3,10 @@ const viewWidth = 1280;
 // 메인 이미지의 넓이
 const mainImgWidth = 400;
 
+let slidePosition = 0;
+let slideIndex = 0;
+let slide = document.querySelectorAll('.slide');
 $(document).ready(() => {
-    let $slide = $('.slide')
-    console.log($slide);
-    
-    let slideMovePosition = 0;
-    
-    let slide = document.querySelectorAll('.slide');
-    console.log(slide);
-
-    let slideIndex = 0;
-
-    // 2초마다 호출되는 이미지 슬라이드 함수
-    setInterval(() => {
-        // 이미지 슬라이드를 다음 슬라이드로 넘긴다
-        slideMovePosition -= mainImgWidth;
-        // 실제로 슬라이드를 넘기는 반복문
-        // 슬라이드 배열 트랜스폼을 다음 슬라이드로 이동시킨다
-        for (let i = 0; i < slide.length; i++) {
-            slide[i].style.transform = `translateX(${slideMovePosition}px)`
-        }
-        slideIndex++;
-        console.log(slideIndex)
-        
-        // 이미지 슬라이드가 마지막 배열을 넘긴다면 처음으로 되돌린다
-        if (slideIndex >= slide.length - 1) {
-            slideMovePosition = mainImgWidth;
-            slideIndex = 0;
-        }
-    }, 2000)
-
 
     // imgShow(slideIndex)
     // setInterval(() => {
@@ -40,6 +14,8 @@ $(document).ready(() => {
     //     slideIndex < $slide.length - 1 ? slideIndex++ : slideIndex = 0;
     //     imgShow(slideIndex)
     // }, 4000);
+
+    // autoSlide(true, 2000)
 })
 
 // 매개변수로 받은 인덱스의 이미지를 보여주는 함수
@@ -50,3 +26,56 @@ const imgShow = (index) => {
     $slide[index].style.display = 'block'
 }
 
+// 이미지 슬라이드를 다음 슬라이드로
+const nextSlide = () => {
+    slideIndex++;
+    // 이미지 슬라이드가 마지막 배열을 넘긴다면 처음으로 되돌린다
+    if (slideIndex >= slide.length) {
+        slidePosition = 0;
+        slideIndex = 0;
+    } else {
+        // 이미지 슬라이드를 다음 슬라이드로 넘긴다
+        slidePosition -= mainImgWidth;
+    }
+
+
+    // 실제로 슬라이드를 넘기는 반복문
+    // 슬라이드 배열 트랜스폼을 다음 슬라이드로 이동시킨다
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.transform = `translateX(${slidePosition}px)`
+    }
+
+
+}
+
+// 이미지 슬라이드를 이전 슬라이드로
+const prevSlide = () => {
+    slideIndex--;
+    // 이미지 슬라이드가 처음 배열을 넘긴다면 마지막으로 되돌린다
+    if (slideIndex < 0) {
+        slideIndex = slide.length - 1;
+        slidePosition -= mainImgWidth * slideIndex;
+    } else {
+        // 이미지 슬라이드를 다음 슬라이드로 넘긴다
+        slidePosition += mainImgWidth;
+    }
+
+    // 실제로 슬라이드를 넘기는 반복문
+    // 슬라이드 배열 트랜스폼을 다음 슬라이드로 이동시킨다
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.transform = `translateX(${slidePosition}px)`
+    }
+}
+
+// 자동으로 다음 슬라이드로
+// @param: time(1000 => 1초)
+// @param: flag(true 반복적으로 호출, false 호출안함)
+const autoSlide = (isIter, time) => {
+    nextSlide();
+    // 2초마다 호출되는 이미지 슬라이드 함수
+    if (isIter) {
+        setInterval(() => {
+            autoSlide()
+        }, time)
+    }
+}

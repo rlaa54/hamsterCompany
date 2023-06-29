3efa517a90092d40141524e37ddba497d49679a5
Yerioh <137850923+Yerioh@users.noreply.github.com>
2023-06-29 09:36:16 +0900

    dd
    dd

--- js/review.js
+++ js/review.js
@@ -1,8 +1,10 @@
 let userTags = [];
+// 별점 리뷰 구현 기능
 const drawStar = (target) => {
     let starRating = Math.floor(target.value / 2);
     $('.star span').css('width', `${starRating * 20}%`);
 }
+// 유저가 입력한 리뷰를 리뷰칸에 추가하는 함수
 const addReview = () => {
     let userInput = $('#user-input').val();
     let starRating = Math.floor($('#star input').val() / 2);
@@ -31,6 +33,7 @@ const addReview = () => {
     console.log(userTags)
     userTags=[];
 }
+//태그를 추가하고 초기화하는 함수
 const addTag = ()=>{
     $('#tag-select').append(`<span>#${$('#tag-input input').val()}</span>`)
     userTags.push(`#${$('#tag-input input').val()}`)
@@ -40,12 +43,14 @@ const resetTag = ()=>{
     $('#tag-select span').remove()
     userTags=[];
 }
+//태그 호버 기능
 $('#tag').on('mouseover',()=>{
     $('#tag-select').css('display','block')
 })
 $('#tag-select').on('mouseleave',()=>{
     $('#tag-select').css('display','none')
-})        
+})
+//클릭이벤트        
 $('#input-btn').on('click',addReview)
 $('#add-btn').on('click',addTag)
 $('#remove-btn').on('click',resetTag)
